4791981bb2cf83b814d5ab50c5d670a8457555e3
syshibayma <shibayama@stailor.co.jp>
2020-06-03 17:59:32 +0900

    SetState

--- src/App.js
+++ src/App.js
@@ -1,37 +1,37 @@
 import React, {Component} from 'react';
 import {render} from "react-dom";
-import Proptypes from 'prop-types';
 
 const App = () => {
-    const profiles = [
-        {name: "Taro", age: 10},
-        {name: "Hanako", age: 100},
-        {name: "NoName", }
-    ]
     return (
-        <div>
-
-            {
-              profiles.map((profile, index) => {
-                return <User name={profile.name} age={profile.age} key={index} />
-                })
-            }
-        </div>
+        <Counter></Counter>
     )
 }
 
-const User = (props) => {
-    return <div>Mow!zz {props.name} , and {props.age} year</div>
+class Counter extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {count: 0}
+    }
+
+    handlePlusButton = () => {
+        this.setState({count: this.state.count + 1})
+    }
+
+    handleMinusButton = () => {
+        this.setState({count: this.state.count - 1})
+    }
+
+
+    render() {
+        return (
+            <React.Fragment>
+                <div>count:{this.state.count}</div>
+                <button onClick={this.handlePlusButton}> +1</button>
+                <button onClick={this.handleMinusButton}> -1</button>
+            </React.Fragment>
+        );
+    }
 }
 
-User.propTypes ={
-    name:Proptypes.string,
-    age:Proptypes.number.isRequired
-}
-
-
-// User.defaultProps ={
-//   age :1
-// }
 
 export default App;
