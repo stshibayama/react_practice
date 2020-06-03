a4d4ecd2c2d0307b423563dd8be7bc091d15ec2a
syshibayma <shibayama@stailor.co.jp>
2020-06-03 16:34:26 +0900

    Component

--- src/App.js
+++ src/App.js
@@ -1,27 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import {render} from "react-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello World!</h1>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// class App extends Component{
+//   render() {
+//     return(
+//     <React.Fragment>
+//       <label htmlFor="bar">Bar</label>
+//       <input type="text" onClick={() => {console.log("Cliked")}} />
+//     </React.Fragment>
+//     )
+//   }
+// }
+
+const App =() => {
+  return <div>
+    <Cat/>
+    <Cat/>
+    <Cat/>
+    <Cat/>
+  </div>
+}
+
+const Cat = () => {
+  return <div>Mow!</div>
 }
 
 export default App;
